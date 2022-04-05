d6c809fa617b45a912fb2f04750070981a35b2cf
Marxdd <63327100+Marxdd@users.noreply.github.com>
2022-04-05 12:51:51 -0700

    Barra de navegacion

--- app/src/main/java/audeves/luis/artphoto/MainActivity.kt
+++ app/src/main/java/audeves/luis/artphoto/MainActivity.kt
@@ -1,11 +1,28 @@
 package audeves.luis.artphoto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import audeves.luis.artphoto.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var iniciarSesion: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iniciarSesion= findViewById(R.id.button)
+        iniciarSesion.setOnClickListener {
+           // Toast.makeText(this,"Prueba",Toast.LENGTH_LONG).show()
+            abrirInicio()
+
+        }
+    }
+
+    fun abrirInicio(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
