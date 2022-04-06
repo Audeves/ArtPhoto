1cd82bb5b5979cc3cedaebee651cead4718dcc87
felipe12007 <felipedejesus1207@gmail.com>
2022-04-05 20:45:02 -0700

    pantalla primer inicio y icono

--- app/src/main/java/audeves/luis/artphoto/MainActivity.kt
+++ app/src/main/java/audeves/luis/artphoto/MainActivity.kt
@@ -4,14 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-import audeves.luis.artphoto.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var iniciarSesion: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_inicio_sesion)
 
         iniciarSesion= findViewById(R.id.button)
         iniciarSesion.setOnClickListener {
