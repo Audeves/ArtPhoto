33971129d7c9969733c86026e18614336d8bf729
Marxdd <63327100+Marxdd@users.noreply.github.com>
2022-04-24 22:19:54 -0700

    Correccion de publicaciones

--- app/src/main/java/audeves/luis/artphoto/Publicacion.kt
+++ app/src/main/java/audeves/luis/artphoto/Publicacion.kt
@@ -1,5 +1,7 @@
 package audeves.luis.artphoto
 
-data class Publicacion (val id: Int,var nombrePublicador: String,var fotoPublicacion: Int, var iconoPerfil: Int){
+
+
+data class Publicacion(val id: Int,var nombrePublicador: String,var fotoPublicacion: Int, var iconoPerfil: Int){
 
 }
