dc8e5e7d888d5fafcb7bd65051ffcceeb111e7aa
Marxdd <63327100+Marxdd@users.noreply.github.com>
2022-04-27 12:17:02 -0700

    Navegacion

--- app/src/main/java/audeves/luis/artphoto/MainActivity.kt
+++ app/src/main/java/audeves/luis/artphoto/MainActivity.kt
@@ -11,11 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
+        var btnIniciarSesion = findViewById<Button>(R.id.button)
+        btnIniciarSesion.setOnClickListener {
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
 
-        iniciarSesion= findViewById(R.id.button)
-        iniciarSesion.setOnClickListener {
-           // Toast.makeText(this,"Prueba",Toast.LENGTH_LONG).show()
-            abrirInicio()
+        }
+        var btnCrearCuenta1 = findViewById<Button>(R.id.btn_crear_cuenta)
+        btnCrearCuenta1.setOnClickListener {
+            val intent: Intent = Intent(this, ActivityCrearCuenta::class.java)
+            startActivity(intent)
 
         }
     }
